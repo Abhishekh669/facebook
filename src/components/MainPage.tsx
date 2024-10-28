c71480a1a867b27438f3f196870dce72f1990108
Abhishekh669 <nepaliabhishekh669@gmail.com>
2024-10-29 02:25:35 +0545

    little fix

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -4,12 +4,12 @@ import { SiMeta } from "react-icons/si";
 
 function MainPage() {
   return (
-    <div className=" h-[100vh] sm:h-[80vh] bg-[#f2f4f7] sm:flex sm:flex-wrap   ">
-      <div className=" h-[40%] p-4 sm:h-full sm:flex-1 sm:flex sm:justify-end">
-        <div className="flex flex-col items-center  p-4 gap-y-32 sm:gap-y-0 sm:justify-center  h-full ">
-          <div className="language text-[19px]w-full ">
+    <div className=" h-[100vh] w-[100vw] sm:h-[80vh] bg-[#f2f4f7] sm:flex sm:flex-row ">
+      <div className=" h-[40%] p-4 sm:h-full sm:flex-1 sm:flex  sm:justify-end">
+        <div className="flex flex-col  items-center  p-4 gap-y-32 sm:gap-y-0 sm:justify-center  h-full ">
+          <div className="language text-[19px] w-full ">
             <span className="sm:hidden  text-black">English(US)</span>
-            <p className="hidden sm:flex  sm:text-[80px] sm:font-semibold sm:text-[#125cef] sm:items-start">
+            <p className="hidden sm:flex  sm:text-[60px] sm:font-semibold sm:text-[#125cef] sm:items-start">
               facebook
             </p>
           </div>
@@ -21,16 +21,16 @@ function MainPage() {
               src="/new.jpeg"
               className="rounded-full h-20 w-20 sm:hidden "
             />
-            <p className="hidden sm:flex sm:flex-col sm:text-[28px] text-black">
+            <p className="hidden sm:flex sm:flex-col sm:text-[23px] text-black">
               <span>Connect with friends and the world </span>
               <span>world around you on Facebook.</span>
             </p>
           </div>
         </div>
       </div>
-      <div className=" w-full h-[60%]   sm:h-full sm:flex-1 pt-4  px-4  flex flex-col justify-around sm:flex-row sm:items-center sm:justify-center sm:p-10 ">
-        <div className="  w-full h-full sm:flex sm:items-center sm:justify-start sm:px-10" >
-          <form action="" className="w-full flex flex-col   gap-y-4 sm:gap-y-5 sm:w-[500px] sm:px-8 sm:pt-6 sm:pb-14 sm:border-2 sm:bg-white sm:rounded-2xl">
+      <div className=" w-full h-[60%]   sm:h-full sm:flex-1 pt-4  px-4  flex flex-col justify-around sm:flex-row sm:items-center sm:justify-center sm:p-0 lg:p-[20px] ">
+        <div className="  w-full h-full sm:flex sm:items-center sm:justify-start lg:px-10" >
+          <form action="" className="w-full flex flex-col   gap-y-4 sm:gap-y-5 sm:w-[250px] md:w-[450px] sm:px-8 sm:pt-6 sm:pb-14 sm:border-2 sm:bg-white sm:rounded-2xl">
             <input
               placeholder="Mobile number or email "
               className="  border-[0.4px] p-[16px] text-black sm:p-[20px] border-gray-400 placeholder:text-gray-500 placeholder:text-[17px] sm:placeholder:text-[20px] rounded-xl focus:outline-[1px] focus:outline-blue-500 "
